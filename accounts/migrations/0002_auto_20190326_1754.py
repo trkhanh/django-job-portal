158c0b42bf70224c965586e075ba4358b41c439a
Tran Khanh <tran_khanh@outlook.com>
2021-03-12 00:27:48 +0700

    implement the account app

---
+++ accounts/migrations/0002_auto_20190326_1754.py
@@ -0,0 +1,21 @@
+import accounts.managers
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('accounts', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterModelManagers(
+            name='user',
+            managers=[
+                ('objects', accounts.managers.UserManager()),
+            ],
+        ),
+        migrations.RemoveField(
+            model_name='user',
+            name='username',
+        ),
+    ]
---
+++ accounts/views.py
@@ -0,0 +1,32 @@
+from django.contrib import messages, auth
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, redirect
+from django.views.generic import CreateView, FormView, RedirectView
+from accounts.forms import *
+from accounts.models import User
+
+
+class RegisterEmployeeView(CreateView):
+    model = User
+    form_class = EmployeeRegistrationForm
+    template_name = "accounts/employee/register.html"
+    success_url = "/"
+
+    extra_context = {"title": "Register"}
+
+    def dispatch(self, request, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return HttpResponseRedirect(self.get_success_url())
+        return super().dispatch(self.request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(data=request.POST)
+
+        if form.is_valid():
+            user = form.save(commit=False)
+            password = form.cleaned_data.get("password1")
+            user.set_password(password)
+            user.save()
+            return redirect("accounts:login")
+        else:
+            return render(request, "account/employee/register.html", {"form": form})
